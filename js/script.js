f48b34b154e19c0688b841b87241523be79ad783
Pavel Orlovsky <paul.fofanov@gmail.com>
2022-06-20 00:38:58 +0300

    AI1

--- js/script.js
+++ js/script.js
@@ -4,9 +4,7 @@ let banner = document.querySelector(".banner");
 let bannerText = banner.querySelector(".banner__text");
 let restartBtn = document.querySelector("#restart-btn");
 
-restartBtn.addEventListener("click", function () {
-  location.reload();
-});
+console.log();
 
 start(cells);
 
@@ -20,19 +18,48 @@ function start(cells) {
       this.style.fontSize = "60px";
       this.removeEventListener("click", step);
       if (isVictory(cells) || i == 8) {
-        banner.classList.toggle("hide");
-        field.classList.toggle("hide");
-        if (isVictory(cells)) {
-          bannerText.innerHTML = `ПОБЕДА! ${
-            this.innerHTML === "X" ? "Крестики" : "Нолики"
-          } победили.`;
-        } else if (i === 8) {
-          bannerText.innerHTML = `НИЧЬЯ! Никто не победил.`;
-        }
+        setTimeout(check, 500, cells, i);
       }
       i++;
+      setTimeout(stepAI, 2000, cells, i, step);
+
+      i++;
+      debugger;
     });
   }
+}
+
+function stepAI(cells, i, step) {
+  /* i++; */
+  if (!isVictory(cells)) {
+    /* console.log(i); */
+    const clickAI = cellAI(cells);
+    actionAI(cells, clickAI, i, step);
+    if (isVictory(cells) || i == 8) {
+      setTimeout(check, 500, cells, i);
+    }
+  }
+
+  /* return i; */
+}
+
+function cellAI(cells) {
+  let click = Math.floor(Math.random() * 9);
+  if (cells[click].innerHTML === "") {
+    return click;
+  } else {
+    return cellAI(cells);
+  }
+}
+
+function actionAI(cells, clickAI, i, step) {
+  cells[clickAI].innerHTML = ["X", "O"][i % 2];
+  cells[clickAI].style.backgroundColor = "rgb(141, 96, 96)";
+  cells[clickAI].style.color = "moccasin";
+  cells[clickAI].style.fontSize = "60px";
+  cells[clickAI].removeEventListener("click", step);
+  cells[clickAI].style.pointerEvents = "none";
+  debugger;
 }
 
 function isVictory(cells) {
@@ -59,3 +86,24 @@ function isVictory(cells) {
   return false;
 }
 
+function check(cells, i) {
+  restartBtn.addEventListener("click", function () {
+    location.reload();
+  });
+  banner.classList.toggle("hide");
+  field.classList.toggle("hide");
+  if (isVictory(cells)) {
+    bannerText.innerHTML = `ПОБЕДА! ${
+      i % 2 === 0 ? "Крестики" : "Нолики"
+    } победили.`;
+  } else if (i === 8) {
+    bannerText.innerHTML = `НИЧЬЯ! Никто не победил.`;
+  }
+}
+
+/* async function stepNumberAI(cells, i, step) {
+  const res = await setTimeout(stepAI, 500, cells, i, step);
+
+  return res;
+}
+ */
