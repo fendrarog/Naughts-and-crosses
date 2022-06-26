702a28dda709f4653bdac079ef375ec4f4a4bd9c
Pavel Orlovsky <paul.fofanov@gmail.com>
2022-06-27 00:11:29 +0300

    new ai

--- js/script.js
+++ js/script.js
@@ -3,52 +3,137 @@ let field = document.querySelector("#field");
 let banner = document.querySelector(".banner");
 let bannerText = banner.querySelector(".banner__text");
 let restartBtn = document.querySelector("#restart-btn");
+let radios = document.querySelectorAll('input[type="radio"]');
+
+let isStarted = false;
+const isDraw = (i) => i === 8;
+
+for (let radio of radios) {
+  radio.addEventListener("click", function (e) {
+    if (isStarted) {
+      location.reload();
+    }
+    if (this.value === "without") {
+      startWithPlayer(cells);
+    } else if (this.value === "with") {
+      startWithAi(cells);
+    }
+    isStarted = true;
+  });
+}
 
 console.log();
 
-start(cells);
-
-function start(cells) {
+function startWithAi(cells) {
   let i = 0;
   for (let cell of cells) {
     cell.addEventListener("click", function step() {
+      field.style.pointerEvents = "none";
+
       this.innerHTML = ["X", "O"][i % 2];
       this.style.backgroundColor = "rgb(141, 96, 96)";
       this.style.color = "moccasin";
       this.style.fontSize = "60px";
       this.removeEventListener("click", step);
-      if (isVictory(cells) || i == 8) {
-        setTimeout(check, 500, cells, i);
+      if (isVictory(cells) || isDraw(i)) {
+        setTimeout(check, 1000, cells, i);
+        debugger;
       }
-      i++;
-      setTimeout(stepAI, 2000, cells, i, step);
 
-      i++;
-      debugger;
+      /*       const ai = (i) => {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve(++i);
+          }, 0);
+        });
+      };
+
+      ai(i).then((i) => {
+        stepAI(cells, i, step);
+        field.style.pointerEvents = "auto";
+      }); */
+
+      function timeout(ms) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+      }
+
+      const ai = async (fn, ...args) => {
+        await timeout(1000);
+        fn(...args);
+        field.style.pointerEvents = "auto";
+      };
+
+      ai(stepAI, cells, i, step);
+
+      i = i + 2;
     });
   }
 }
 
 function stepAI(cells, i, step) {
-  /* i++; */
+  i++;
   if (!isVictory(cells)) {
-    /* console.log(i); */
     const clickAI = cellAI(cells);
+    debugger;
     actionAI(cells, clickAI, i, step);
-    if (isVictory(cells) || i == 8) {
-      setTimeout(check, 500, cells, i);
+    if (isVictory(cells) || isDraw(i)) {
+      setTimeout(check, 1000, cells, i);
     }
   }
-
-  /* return i; */
 }
 
-function cellAI(cells) {
+/* function cellAI(cells) {
   let click = Math.floor(Math.random() * 9);
   if (cells[click].innerHTML === "") {
     return click;
   } else {
     return cellAI(cells);
+  }
+} */
+
+function cellAI(cells) {
+  let winLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  let breakComb = winLines.reduce((acc, curr) => {
+    debugger;
+
+    if (
+      curr.filter((el) => cells[el].innerHTML === "X").length === 2 &&
+      curr.some((el) => cells[el].innerHTML === "")
+    ) {
+      acc = curr.filter((el) => cells[el].innerHTML === "")[0];
+      debugger;
+    }
+    return acc;
+  }, 0);
+
+  if (breakComb) {
+    return breakComb;
+  }
+
+  if (cells[4].innerHTML === "") {
+    return 4;
+  }
+
+  let corners = [0, 2, 6, 8];
+  let haveEmptyCorner = corners.some(
+    (corner) => cells[corner].innerHTML === ""
+  );
+
+  if (haveEmptyCorner) {
+    let emptyCorners = corners.filter(
+      (corner) => cells[corner].innerHTML === ""
+    );
+    return emptyCorners[0];
   }
 }
 
@@ -59,7 +144,6 @@ function actionAI(cells, clickAI, i, step) {
   cells[clickAI].style.fontSize = "60px";
   cells[clickAI].removeEventListener("click", step);
   cells[clickAI].style.pointerEvents = "none";
-  debugger;
 }
 
 function isVictory(cells) {
@@ -101,9 +185,43 @@ function check(cells, i) {
   }
 }
 
-/* async function stepNumberAI(cells, i, step) {
-  const res = await setTimeout(stepAI, 500, cells, i, step);
-
-  return res;
+function startWithPlayer(cells) {
+  let i = 0;
+  for (let cell of cells) {
+    cell.addEventListener("click", function step() {
+      this.innerHTML = ["X", "O"][i % 2];
+      this.style.backgroundColor = "rgb(141, 96, 96)";
+      this.style.color = "moccasin";
+      this.style.fontSize = "60px";
+      this.removeEventListener("click", step);
+      if (isVictory(cells) || i == 8) {
+        setTimeout(check, 1000, cells, i);
+      }
+      i++;
+    });
+  }
 }
- */
+
+function isVictory(cells) {
+  let winLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let winLine of winLines) {
+    if (
+      cells[winLine[0]].innerHTML === cells[winLine[1]].innerHTML &&
+      cells[winLine[1]].innerHTML === cells[winLine[2]].innerHTML &&
+      cells[winLine[0]].innerHTML !== ""
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
